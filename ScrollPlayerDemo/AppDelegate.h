7e86c7dcb0e9ab93c2d724fbfb323b040c684523
曹檑 <1090491594@qq.com>
2017-10-20 09:48:24 +0800

    Initial Commit

---
+++ ScrollPlayerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollPlayerDemo
+//
+//  Created by Zeus on 2017/10/20.
+//  Copyright © 2017年 Zeus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScrollPlayerDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ScrollPlayerDemo
+//
+//  Created by Zeus on 2017/10/20.
+//  Copyright © 2017年 Zeus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
